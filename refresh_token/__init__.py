0587a265db51a5f8b8a3a57860052620820468bf
Aaron <kljsandjb@me.com>
2020-10-18 18:45:28 +0200

    rm CRLF

--- refresh_token/__init__.py
+++ refresh_token/__init__.py
@@ -16,22 +16,23 @@ def main(mytimer: func.TimerRequest) -> None:
     refresh_token = os.environ["host"] + "token"
     request = urllib.request.Request(refresh_token)
     request.add_header("Content-Type", "application/x-www-form-urlencoded")
-        
+
     # get refresh token from table
     connection_string = os.environ["AzureWebJobsStorage"]
     client = TableClient.from_connection_string(conn_str=connection_string, table_name="tokens")
-    entity = client.get_entity(partition_key="pk", row_key="rk")
+    try:
+        entity = client.get_entity(partition_key="pk", row_key="rk")
+        # the request body should include refresh token
+        body = {"client_id": os.environ["client_id"], "scope": "User.ReadBasic.All", "refresh_token": entity.refresh_token.value, "redirect_uri": os.environ["redirect_uri"], 
+                "grant_type": "refresh_token", "client_secret": os.environ["client_secret"]}
+        response = urllib.request.urlopen(request,  data=urlencode(body, quote_via=quote_plus).encode("utf-8"))
+        res_body = json.loads(response.read().decode())
 
-    # the request body should include refresh token
-    body = {"client_id": os.environ["client_id"], "scope": "User.ReadBasic.All", "refresh_token": entity.refresh_token.value, "redirect_uri": os.environ["redirect_uri"], 
-            "grant_type": "refresh_token", "client_secret": os.environ["client_secret"]}
-    response = urllib.request.urlopen(request,  data=urlencode(body, quote_via=quote_plus).encode("utf-8"))
-    res_body = json.loads(response.read().decode())
+        # update tokens in the table
+        entity.access_token = res_body["access_token"]
+        entity.refresh_token = res_body["refresh_token"]
+        client.update_entity(mode=UpdateMode.REPLACE, entity=entity)
+        logging.info('tokens were refreshed at %s', utc_timestamp)
+    except HttpResponseError:
+        logging.info('tokens are not generated yet, will try again later.')
 
-    # update tokens in the table
-    entity.access_token = res_body["access_token"]
-    entity.refresh_token = res_body["refresh_token"]
-    client.update_entity(mode=UpdateMode.REPLACE, entity=entity)
-#     print(entity.access_token)
-
-    logging.info('tokens were refreshed at %s', utc_timestamp)
